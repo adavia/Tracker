39a1013574f8b80a7fcb7d2ff56d820d4c3362f6
adavia <andresi.davia@gmail.com>
2016-01-31 01:15:04 -0500

    Display a comment's state transition

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :ticket
   belongs_to :author, class_name: "User"
+  belongs_to :previous_state, class_name: "State"
   belongs_to :state
 
   validates :text, presence: true
 
+  before_create :set_previous_state
   after_create :set_ticket_state
 
   delegate :project, to: :ticket
@@ -12,6 +14,10 @@ class Comment < ActiveRecord::Base
   scope :persisted, lambda { where.not(id: nil) }
 
   private
+    def set_previous_state
+      self.previous_state = ticket.state
+    end
+
     def set_ticket_state
       ticket.state = state
       ticket.save!
